f3870cc1efbec0dc26f9329fa039b92541d0fb0c
saloni-saxena <salonis@andrew.cmu.edu>
2018-09-20 20:41:23 -0400

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,145 +1,63 @@
-
-
-// JavaScript Document
-
-$('#subscribeform').submit(function(){
-
-
-
-	var action = $(this).attr('action');
-
-
-
-		$("#mesaj").slideUp(750,function() {
-
-
-
-
-
-
-
-		$('#mesaj').hide();
-
-
-
-	$('#subsubmit')
-
-
-
-
-
-
-
-			.after('')
-
-
-
-
-
-
-
-			.attr('disabled','disabled');
-
-
-
-	$.post(action, {
-
-
-
-
-
-
-
-			email: $('#subemail').val()
-
-
-
-
-
-
-
-		},
-
-
-
-
-
-
-
-			function(data){
-
-
-
-
-
-
-
-				document.getElementById('mesaj').innerHTML = data;
-
-
-
-
-
-
-
-				$('#mesaj').slideDown('slow');
-
-
-
-
-
-
-
-				$('#subscribeform img.subscribe-loader').fadeOut('slow',function(){$(this).remove()});
-
-
-
-
-
-
-
-				$('#subsubmit').removeAttr('disabled');
-
-
-
-
-
-
-
-				if(data.match('success') != null) $('#subscribeform').slideUp('slow');
-
-
-
-			}
-
-
-
-
-
-
-
-		);
-
-
-
-
-
-
-
-		});
-
-
-
-
-
-
-
-		return false;
-
-
-
-
-
-
-
-	});
+$(window).load(function () {
+
+    // preloader
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(550).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(550).css({
+        'overflow': 'visible'
+    });
+
+
+    //  isotope
+    var $container = $('.portfolio_container');
+    $container.isotope({
+        filter: '*',
+    });
+
+    $('.portfolio_filter a').click(function () {
+        $('.portfolio_filter .active').removeClass('active');
+        $(this).addClass('active');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 500,
+                animationEngine: "jquery"
+            }
+        });
+        return false;
+    });
+
+    // back to top
+    var offset = 300,
+        offset_opacity = 1200,
+        scroll_top_duration = 700,
+        $back_to_top = $('.cd-top');
+
+    //hide or show the "back to top" link
+    $(window).scroll(function () {
+        ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if ($(this).scrollTop() > offset_opacity) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
+
+    //smooth scroll to top
+    $back_to_top.on('click', function (event) {
+        event.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0,
+        }, scroll_top_duration);
+    });
+
+    // input
+    $(".input-contact input, .textarea-contact textarea").focus(function () {
+        $(this).next("span").addClass("active");
+    });
+    $(".input-contact input, .textarea-contact textarea").blur(function () {
+        if ($(this).val() === "") {
+            $(this).next("span").removeClass("active");
+        }
+    });
+});
